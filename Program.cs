1210aff542a0a66bb34030e7e93176b948d02a83
Ciocolici <68241693+Ciocolici@users.noreply.github.com>
2023-09-04 12:27:04 +0200

    added the project

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Der User soll angeben können, wieviele Zahlen der Fibonacci-Folge er berechnet haben möchte. 
+            //Gib in der Konsole dann die entsprechenden Fibonacci-Zahlen aus.
+            //Die Fibonacci-Folge beginnt mit den Zahlen 0 und 1. Jede weitere Zahl ergibt sich, in dem man die beiden vorherigen Zahlen zusammenzählt. 
+            //Als Beispiel die ersten zehn Zahlen:
+            //0 1 1 2 3 5 8 13 21 34
+
+            Console.WriteLine("Wie viele Zählen der Fibonacci-Folge möchtest du berechnet haben?");
+            string userZahlString = Console.ReadLine();
+            int userZahl = Convert.ToInt32(userZahlString);
+
+            ulong j = 0;
+            ulong k = 1;
+
+            for (int i = 0; i < userZahl; i++)
+            {
+                Console.WriteLine(j);
+                Thread.Sleep(1000);
+
+                k = j + k;
+                j = k - j;
+            }
+        }
+    }
+}
